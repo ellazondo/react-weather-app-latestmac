cd01352627dfbf341f4d02a6471aab514c469598
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2022-12-09 14:10:19 -0500

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Gabriella Elizondo and is{" "}
-        <a href="https://github.com/ellazondo/react-weather-app-new-mac">
-          open sourced on Github
-        </a>
-      </footer>
+      <div ClassName="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Gabriella Elizondo and is{" "}
+          <a href="https://github.com/ellazondo/react-weather-app-new-mac">
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
