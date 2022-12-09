f8ccd818ea3ed4cfa3cd58e7037210486a090511
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2022-12-09 12:31:48 -0500

    add space

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by Gabriella Elizondo and is{""}
+        This project was coded by Gabriella Elizondo and is{" "}
         <a href="https://github.com/ellazondo/react-weather-app-new-mac">
           open sourced on Github
         </a>
